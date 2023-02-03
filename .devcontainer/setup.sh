3417dbca1f31f963ecacabcb46cd15357ed3a0f0
Carlo <psielisorigin@gmail.com>
2023-02-03 18:09:24 +0000

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-pip3 install --user -r requirements.txt
+pip3 install --user -r .devcontainer/requirements.txt
+
+mim install mmcv-full
+mim install mmdet
+
+git clone https://github.com/open-mmlab/mmrotate.git
+cd mmrotate
+pip install -e .
